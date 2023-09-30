f0e6b6c5c2f5b03d7244cea8f0a2430ab12e4113
Deisy Moreno <52799752+deisymorlop1103@users.noreply.github.com>
2023-09-29 20:56:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+//bucleforEach con arreglo de letras
+
 const letters = [ "a", "b", "c","d"]
 const f = function() {
   console.logesta es la letra: " + letter );
 };
 
 letters.forEach(f);
+
+//bucle forEach para sacar números primos del 1 al 40
+
+function validaPrimo(numero) {
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const arreglo_primos = [];
+
+for (let i = 2; i <= 40; i++) {
+  if (validaPrimo(i)) {
+     arreglo_primos.push(i);
+  }
+}
+
+primos.forEach(function(numero_primo) {
+  console.log(numero_primo);
+});
